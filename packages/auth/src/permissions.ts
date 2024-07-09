860deef5f6e7f825387161ef570aba9ff613b0b4
Maria de Fátima <mfatima5bc@gmail.com>
2024-07-08 23:45:45 -0300

    wip: typing permissions and subjects

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -2,8 +2,7 @@ import { AbilityBuilder } from '@casl/ability'
 
 import { AppAbility } from '.'
 import { User } from './models/user'
-
-type Role = 'ADMIN' | 'MEMBER'
+import { Role } from './roles'
 
 type PermissionsByRole = (
 	user: User,
@@ -15,7 +14,8 @@ export const permissions: Record<Role, PermissionsByRole> = {
 		can('manage', 'User')
 	},
 	MEMBER(_, { can }) {
-		can('invite', 'User')
-		can('create', 'Project')
+		can('manage', 'Project')
+		can('transfer_ownership', 'Organization')
 	},
+	BILLING() {},
 }
