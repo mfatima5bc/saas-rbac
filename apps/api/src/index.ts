469bd469c03978902459f5532e57dbf6e580bffb
Maria de Fátima <mfatima5bc@gmail.com>
2024-08-10 10:54:18 -0300

    wip: Define app base server   - define app server   - define first route to create account   - create prisma connection for app

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbilityFor } from '@saas/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER' })
-
-const userCanInviteOtherUser = ability.can('invite', 'User')
-const usercantDelteOtherUser = ability.can('delete', 'User')
-
-console.log(userCanInviteOtherUser)
-console.log(usercantDelteOtherUser)
