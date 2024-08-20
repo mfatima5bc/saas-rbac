f1c2ac9aa65d17aad66ae2f51ddb1cedda479e3b
Maria de Fátima <mfatima5bc@gmail.com>
2024-08-19 22:46:22 -0300

    feat: implements route to create organization

--- apps/api/src/http/routes/auth/get-profile.ts
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -17,7 +17,7 @@ export async function getProfile(app: FastifyInstance) {
 				schema: {
 					tags: ['Auth'],
 					summary: 'Get authenticated user profile',
-					security: [{ bearerAuth: []}],
+					security: [{ bearerAuth: [] }],
 					response: {
 						200: z.object({
 							user: z.object({
