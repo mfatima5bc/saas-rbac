9d72c4b473b1538673bda9295c0f7425956508b1
Maria de Fátima <mfatima5bc@gmail.com>
2024-08-25 21:13:31 -0300

    feat: remove member from organization

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -19,6 +19,7 @@ import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
 import { getMembers } from './routes/members/get-members'
+import { removeMember } from './routes/members/remove-member'
 import { updateMember } from './routes/members/update-member'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getOrganization } from './routes/orgs/get-organization'
@@ -97,6 +98,7 @@ app.register(getProject)
 app.register(getProjects)
 app.register(getMembers)
 app.register(updateMember)
+app.register(removeMember)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
 	console.log('HTTP server is running!')
