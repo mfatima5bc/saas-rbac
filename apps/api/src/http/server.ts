12e303c945ea3a7d7e18295f2703028264dbb3a3
Maria de Fátima <mfatima5bc@gmail.com>
2024-08-25 21:44:41 -0300

    feat: accept and reject invite

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,7 +18,9 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { acceptInvite } from './routes/invites/accept-invite'
 import { createInvite } from './routes/invites/create-invite'
+import { getInvite } from './routes/invites/get-invite'
 import { getMembers } from './routes/members/get-members'
 import { removeMember } from './routes/members/remove-member'
 import { updateMember } from './routes/members/update-member'
@@ -101,6 +103,8 @@ app.register(getMembers)
 app.register(updateMember)
 app.register(removeMember)
 app.register(createInvite)
+app.register(getInvite)
+app.register(acceptInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
 	console.log('HTTP server is running!')
