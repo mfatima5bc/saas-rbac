5abc5634c7b2895dc21bb9e5118fb27f340e5211
Maria de Fátima <mfatima5bc@gmail.com>
2024-08-25 21:10:16 -0300

    feat: update member role route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -19,6 +19,7 @@ import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
 import { getMembers } from './routes/members/get-members'
+import { updateMember } from './routes/members/update-member'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getOrganization } from './routes/orgs/get-organization'
 import { getOrganizations } from './routes/orgs/get-organizations'
@@ -95,6 +96,7 @@ app.register(deleteProject)
 app.register(getProject)
 app.register(getProjects)
 app.register(getMembers)
+app.register(updateMember)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
 	console.log('HTTP server is running!')
