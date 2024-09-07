520c537c1696d6e1c1a0c1f2bf350a5284b2c693
Maria de Fátima <mfatima5bc@gmail.com>
2024-09-07 11:51:15 -0300

    wip: user permissions

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -2,11 +2,13 @@ import { Slash } from 'lucide-react'
 import Image from 'next/image'
 
 import satelliteIcon from '@/assets/satellite-svgrepo.svg'
+import { ability } from '@/auth/auth'
 
 import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
-export function Header() {
+export async function Header() {
+	const permissions = await ability()
 	return (
 		<div className="mx-auto flex max-w-[1200px] items-center justify-between">
 			<div className="flex items-center gap-3">
@@ -15,11 +17,11 @@ export function Header() {
 					className="size-6 dark:invert"
 					alt="Rocketseat"
 				/>
+				<Slash className="size-3 -rotate-[24deg] text-border" />
+
+				<OrganizationSwitcher />
+				{permissions?.can('get', 'Project') && <p>Project</p>}
 			</div>
-
-			<Slash className="size-3 -rotate-[24deg] text-border" />
-
-			<OrganizationSwitcher />
 
 			<div className="flex items-center gap-4">
 				<ProfileButton />
