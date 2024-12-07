bbc81f80724ff4f68a77372259a8afc731e9049f
Maria de Fátima <mfatima5bc@gmail.com>
2024-12-07 17:53:10 -0300

    fix: theme selected

--- apps/web/src/components/header-icon.tsx
+++ apps/web/src/components/header-icon.tsx
@@ -6,11 +6,11 @@ import { useTheme } from 'next-themes'
 import satelliteIcon from '@/assets/satellite-svgrepo.svg'
 
 export default function HeaderLogoIcon() {
-	const { theme } = useTheme()
+	const { resolvedTheme } = useTheme()
 	return (
 		<Image
 			src={satelliteIcon}
-			className={theme === 'dark' ? 'size-6 dark:invert' : 'size-6'}
+			className={resolvedTheme === 'dark' ? 'size-6 dark:invert' : 'size-6'}
 			alt="Rocketseat"
 		/>
 	)
